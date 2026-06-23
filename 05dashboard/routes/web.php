bb5328d200e3b76c676bb8db38083cb09bdd82a7
Dennis Vera <dennis.vera.u@gmail.com>
2026-06-23 12:47:21 -0400

    cahnge in web api

--- 05dashboard/routes/web.php
+++ 05dashboard/routes/web.php
@@ -25,3 +25,4 @@ Route::get('/productos',[ControllerEstudiante::class,'metodoproductos']);
 
 
 Route::get('/id/categoria/timestamp',[ControllerEstudiante::class,'metodocategorias']);
+Route::get('/id/categoria/timestamp',[ControllerEstudiante::class,'metodovhspulin']);
